a37b5538a1447b12e083b73d8fc364bd2c2a8c9f
Paul03052002 <pavlo.hontar.mv.2019@lpnu.ua>
2026-06-01 22:01:49 +0300

    Add files via upload

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,8 @@
 function getElementWidth(content,padding,border) {
-    const totalWidth = content + 2 * padding + 2 * border;
+    const contentValue = parseFloat(content);
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+    const totalWidth = contentValue + 2 * paddingValue + 2 * borderValue;
     return totalWidth
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
